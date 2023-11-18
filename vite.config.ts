942ce50d16181fede045a760b015be01b510466e
Manoj kumar P Benni <manojpbimp@gmail.com>
2023-11-18 17:00:30 +0530

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
@@ -14,5 +13,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), tsconfigPaths()]
-})
+  plugins: [react(), tsconfigPaths()],
+});
